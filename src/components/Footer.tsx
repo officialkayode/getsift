797c108f3386d8fa79f90f9b62abe151e3999094
officialkayode <officialkayode@gmail.com>
2025-01-02 17:52:07 -0800

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+export const Footer = () => {
+  return (
+    <footer className="border-t border-slate-700 py-8 bg-slate-900">
+      <div className="container px-4">
+        <p className="text-center text-slate-400">
+          © 2024 Sift. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
