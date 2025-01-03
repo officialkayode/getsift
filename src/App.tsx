fa69075d738906e04b52e17cdfe10417aaffe94d
officialkayode <officialkayode@gmail.com>
2025-01-02 20:36:58 -0800

    removing navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { NavigationBar } from "./components/NavigationBar";
 import Index from "./pages/Index";
-import UseCases from "./pages/UseCases";
 
 const queryClient = new QueryClient();
 
@@ -15,10 +13,8 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <NavigationBar />
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/use-cases" element={<UseCases />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,13 +1,13 @@
-import { HeroSection } from "@/components/HeroSection.tsx";
+import { HeroSection } from "@/components/HeroSection";
 import { InnovativeSolutions } from "@/components/InnovativeSolutions";
 import { ValueProposition } from "@/components/ValueProposition";
 import { CompetitiveLandscape } from "@/components/CompetitiveLandscape";
 import { CTASection } from "@/components/CTASection";
-import { Footer } from "@/components/Footer.tsx";
+import { Footer } from "@/components/Footer";
 
 const Index = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-white to-[#f8f8f8] text-[#272525] pt-20">
+    <div className="min-h-screen bg-gradient-to-b from-white to-[#f8f8f8] text-[#272525]">
       <div className="min-h-screen">
         <HeroSection />
         <InnovativeSolutions />
